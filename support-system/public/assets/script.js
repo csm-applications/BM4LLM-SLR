b169b0870a185ab22e6169f604006a593d354cba
Vinicius dos Santos <vinistos@gmail.com>
2025-08-22 11:15:26 -0300

    atualiza tool

--- support-system/public/assets/script.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("agreeBtn").addEventListener("click", function() {
-  window.location.href = "instructions.html"; // redireciona para a próxima etapa
-});
-
-document.getElementById("disagreeBtn").addEventListener("click", function() {
-  alert("You have chosen not to participate in the experiment. Thank you!");
-});
